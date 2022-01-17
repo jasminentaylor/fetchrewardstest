178618d42de5eb947068a44f27087b66314fed6b
Jasmine Taylor <jasminentaylor91@gmail.com>
2022-01-16 21:06:00 -0500

    exercise complete

--- code.js
+++ code.js
@@ -1,1 +1,14 @@
-GET /index.html 
+let button = document.getElementById('submit_button')
+let userInfo = []
+
+button.addEventListener('click', function(event) {
+    event.preventDefault()
+   document.body.append('Welcome!')
+})
+
+fetch ('https://frontend-take-home.fetchrewards.com/form')
+  .then((response) => response.json())
+  .then(data => {
+      console.log(data); 
+      userInfo = data 
+  }) 
